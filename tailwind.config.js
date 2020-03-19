8b38a422031bf7a0c699183776eac4a869f7558c
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-19 16:54:54 -0600

    adding tailwind config and the beginnings of a color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      primary: "var(--color-primary)",
+      secondary: "var(--color-secondary)",
+      success: "var(--color-success)",
+      danger: "var(--color-danger)",
+      warning: "var(--color-warning)",
+      info: "var(--color-info)"
+    }
   },
   variants: {},
   plugins: []
