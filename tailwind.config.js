1d88d6e9e41d1c90c0ad848d29b886a9c67bdd25
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-20 12:12:34 -0600

    Tailwind (#3)
    * making the navbar fixed
    
    * fixing navbar height variable implementation
    
    * adding z-index
    
    * initially adding tailwind
    
    * adding tailwind config and the beginnings of a color scheme
    
    * initial working theme
    
    * skipping theming for now, just tailwind
    
    * some more cleanup after implementing tailwind (there will be even more in the future)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: "var(--color-primary)",
+      secondary: "var(--color-secondary)",
+      danger: "var(--color-danger)",
+      warning: "var(--color-warning)",
+      info: "var(--color-info)",
+      background: "var(--color-background)",
+      white: "var(--color-white)",
+      black: "var(--color-black)",
+      overlay: "var(--color-overlay)"
+    }
+  },
+  variants: {},
+  plugins: []
+};
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,17 @@ module.exports = webpackConfigEnv => {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"]
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: [require("tailwindcss"), require("autoprefixer")]
+              }
+            }
+          ]
         }
       ]
     }
