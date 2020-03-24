b15d16f866e9c1e3af9c9509dc2fe8f0de061d2d
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-24 14:21:14 -0600

    fixing export

--- src/react-mf-styleguide.js
+++ src/react-mf-styleguide.js
@@ -1,7 +1,5 @@
 import "./set-public-path";
 import "./global.css?modules=false";
-import _Button from "./button.component.js";
 
 // You can also export React components from this file and import them into your microfrontends
-
-export const Button = _Button;
+export { default as Button } from "./button.component.js";
