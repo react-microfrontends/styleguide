e534c6ac8c3e362b05e8b5a6db06e28d8ea92461
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-24 14:31:17 -0600

    allowing className customizations

--- src/button.component.js
+++ src/button.component.js
@@ -5,13 +5,14 @@ export default function Button(props) {
     children,
     disabled = false,
     loading = false,
+    className,
     ...remainingProps
   } = props;
   const background =
     disabled || loading ? "opacity-50 bg-secondary" : "bg-warning";
   return (
     <button
-      className={`mb-8 font-bold py-2 px-4 rounded ${background}`}
+      className={`mb-8 font-bold py-2 px-4 rounded ${background} ${className}`}
       disabled={disabled || loading}
       {...remainingProps}
     >
