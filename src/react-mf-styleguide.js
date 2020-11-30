2d1b558b2d843eedbf82364dd68032097d049c6f
Joel Denning <joeldenning@gmail.com>
2020-11-29 21:06:54 -0700

    Removing set-public-path file

--- src/react-mf-styleguide.js
+++ src/react-mf-styleguide.js
@@ -1,4 +1,4 @@
-import "./set-public-path";
+import "systemjs-webpack-interop/auto-public-path";
 import "./global.css";
 
 // You can also export React components from this file and import them into your microfrontends
